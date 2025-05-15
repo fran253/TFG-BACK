0f9206ee4aab10171d27fbb4e786691d23f80d74
fran253 <123721486+fran253@users.noreply.github.com>
2025-05-15 13:13:44 +0200

    Update MarcadorRequest.cs

--- DTOs/Marcador/MarcadorRequest.cs
+++ DTOs/Marcador/MarcadorRequest.cs
@@ -2,7 +2,8 @@ namespace TuProyecto.Api.DTOs.Marcador
 {
     public class MarcadorRequest
     {
-        public decimal MinutoImportante { get; set; } 
+        public decimal MinutoImportante { get; set; }
         public string Titulo { get; set; }
     }
+
 }
