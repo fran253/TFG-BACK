6abdd41533d92bbbb50b124d52778561c394fac7
Fran <a25618@svalero.com>
2025-06-03 18:03:43 +0000

    ruta

--- IA/server.js
+++ IA/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"], // Permitir frontend
+  origin: ["http://34.198.50.70:8080", "http://34.198.50.70:5000"], // Permitir frontend
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
